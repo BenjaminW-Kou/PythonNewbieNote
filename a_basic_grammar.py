821326e856777f2986e120db19bb9f728513a249
BenJaminWKou <BenjaminWKou@gmail.com>
2020-06-11 23:29:15 +0800

    '基本数据类型与变量的定义'

---
+++ a_basic_grammar.py
@@ -0,0 +1,38 @@
+# 1. 基本数据类型(整数-int, 浮点数-float, 字符串-str, 布尔值-bool)
+# 1.1 整数 int
+print(type(1))
+print(type(2))
+# 1.2 浮点数 float
+print(type(1.1))
+print(type(2.0))
+# 1.3 字符串 str
+print(type('python'))
+print(type("python"))
+# 1.4 布尔值
+print(type(True))
+print(type(False))
+# 1.5 str转int
+print(type(int('8')))
+# 1.6 float转str
+print(type(str(2.0)))
+# 1.7 int转bool
+print(bool(123))
+print(bool(0))
+print(bool(-321))
+
+# 2 变量的定义和常用操作
+a = 12
+b = a
+name, age, sex = False, '5', 3.3
+print(name)
+print(age)
+print(sex)
+# 一般使用a,b,c...z,这种命名方式为临时变量
+# 命名要有意义
+# 命名规范
+# 小驼峰
+firstName: str
+# 大驼峰
+LastName: str
+# 连接线
+id_card: str
